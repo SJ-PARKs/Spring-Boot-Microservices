908fbdbc893f659270a83befc674d683e60393d3
박성준 <sungjunp1208@gmail.com>
2025-05-11 20:09:14 +0900

    restructure project folder

--- product-service/src/main/java/com/devdi/microservices/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devdi.microservices.product.repository;
-
-import com.devdi.microservices.product.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
