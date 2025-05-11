7ad8ff5dc62f7d1492a071f05bed1a2acf44ac93
박성준 <sungjunp1208@gmail.com>
2025-05-11 19:17:50 +0900

    build product service

---
+++ src/main/java/com/devdi/microservices/product/dto/ProductRequest.java
@@ -0,0 +1,7 @@
+package com.devdi.microservices.product.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(String id, String name, String description, BigDecimal price) {
+
+}
---
+++ src/main/java/com/devdi/microservices/product/dto/ProductResponse.java
@@ -0,0 +1,7 @@
+package com.devdi.microservices.product.dto;
+
+import java.math.BigDecimal;
+
+public record ProductResponse(String id, String name, String description, BigDecimal price) {
+
+}
