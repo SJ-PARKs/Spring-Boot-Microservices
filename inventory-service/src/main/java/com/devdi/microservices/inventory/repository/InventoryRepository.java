c6140e104e5dda459d63166a94b934b11487706c
박성준 <sungjunp1208@gmail.com>
2025-05-16 22:59:53 +0900

    build inventory service

---
+++ inventory-service/src/main/java/com/devdi/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.devdi.microservices.inventory.repository;
+
+import com.devdi.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
