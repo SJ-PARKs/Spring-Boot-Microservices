96e065d608e16d54138c191ca1a70b3eb862acf9
박성준 <sungjunp1208@gmail.com>
2025-05-17 22:36:29 +0900

    add inventory logic

--- order-service/src/main/java/com/devdi/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/devdi/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.devdi.microservices.order.service;
 
+import com.devdi.microservices.order.client.InventoryClient;
 import com.devdi.microservices.order.dto.OrderRequest;
 import com.devdi.microservices.order.model.Order;
 import com.devdi.microservices.order.repository.OrderRepository;
@@ -11,15 +12,24 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
 
-    private final OrderRepository orderRepository;;
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest orderRequest){
-        //map OrderRequest to Order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        //save order to OrderRepository
-        orderRepository.save(order);
+        var isProductInStock=inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            //map OrderRequest to Order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            //save order to OrderRepository
+            orderRepository.save(order);
+
+        }else{
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
