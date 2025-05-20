c0f408c97fbc2b2054744a62b57ce784a5111099
박성준 <sungjunp1208@gmail.com>
2025-05-21 01:44:29 +0900

    refactor openfeign to webclient

--- order-service/src/main/java/com/devdi/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/devdi/microservices/order/config/RestClientConfig.java
@@ -1,2 +1,29 @@
-package com.devdi.microservices.order.config;public class RestClientConfig {
+package com.devdi.microservices.order.config;
+
+import com.devdi.microservices.order.client.InventoryClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+@RequiredArgsConstructor
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
 }
